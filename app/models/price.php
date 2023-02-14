50b0a73046e0eb5ecbc48695dccddfab537e58eb
I-Jurij <yjurij@gmail.com>
2023-02-14 22:22:18 +0300

    added price

--- app/models/price.php
+++ app/models/price.php
@@ -1,5 +1,79 @@
 <?php
 namespace App\Models;
+use \App\Lib\Registry;
 class Price extends Home
 {
+    public function get_data($path)
+	{
+		$this->data['nav'] = Registry::get('nav');
+
+		if ( null !== Registry::get('page_db_data') ) {
+			$this->data['page_db_data'] = Registry::get('page_db_data');
+		} else {
+			$this->db_query();
+		}
+		//add css for head in template
+		$this->data['css'] = $this->css_add('public'.DS.'css'.DS.'first');
+
+		$i = 0;
+		foreach ($this->db->db->select("contacts", ["contacts_type", "contacts_data"]) as $value) {
+			if ($value['contacts_type'] === 'tlf' && !empty($value['contacts_data'])) {
+				$this->data['tlf'.$i] = $value['contacts_data'];
+				$i++;
+			} elseif (!empty($value['contacts_data'])) {
+				$this->data[$value['contacts_type']] = $value['contacts_data'];
+			}
+		}
+
+        //get page of services list from db
+		$data['service_page'] = $this->db->db->select($this->table, [
+            "page_id",
+            "page_title"
+        ], [
+            "page_publish[!]" => null
+        ]);
+
+        $data['page_cats'] = $this->db->db->select("serv_categories", [
+            "id",
+            "page_id",
+            "category_name"
+        ]);
+
+        $data['page_cats_serv'] = $this->db->db->select("services", [
+            "id",
+            "page_id",
+            "category_id",
+            "service_name",
+            "price"
+        ], [
+            "category_id[!]" => null
+        ]);
+
+        $data['page_serv'] = $this->db->db->select("services", [
+            "id",
+            "page_id",
+            "service_name",
+            "price"
+        ], [
+            "category_id" => null
+        ]);
+
+        foreach ($data['service_page'] as $page) {
+            foreach ($data['page_cats'] as $cat) {
+                if ($cat['page_id'] === $page['page_id']) {
+                    foreach ($data['page_cats_serv'] as $cat_serv) {
+                        if ($cat_serv['category_id'] === $cat['id']) {
+                            $this->data['serv'][$page['page_title']][$cat['category_name']][$cat_serv['service_name']] = $cat_serv['price'];
+                        }
+                    }
+                }
+            }
+            foreach ($data['page_serv'] as $serv) {
+                if ($serv['page_id'] === $page['page_id']) {
+                    $this->data['serv'][$page['page_title']]['page_serv'][$serv['service_name']] = $serv['price'];
+                }
+            }
+        }
+		return $this->data;
+	}
 }
--- app/view/price.php
+++ app/view/price.php
@@ -1,10 +1,64 @@
-<div class="content">
+<div class="content"><p>Стоимость услуг в нерабочее время или дни, а также при выезде - договорная.</p></div>
+
+<div class=" margin_rlb1">
 <?php
-if (!empty($data['res'])) {
-print $data['res'];
-include_once APPROOT.DS."view".DS."js_back.html";
-} else {
-print 'start';
-include_once APPROOT.DS."view".DS."back_home.html";
-}?>
+foreach ($data['serv'] as $page => $cat_arr) {
+    $i = 1;
+    ?>
+    <div class="back shad rad pad margin_rlb1 price" id="<?php echo $page; ?>">
+        <table class="table">
+            <caption class=""><h2><?php echo $page; ?></h2></caption>
+            <colgroup>
+            <col width="10%">
+            <col width="65%">
+            <col width="25%">
+            </colgroup>
+            <thead>
+            <tr>
+                <th>№</th>
+                <th>Услуга</th>
+                <th>Цена, руб.</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+            foreach ($cat_arr as $cat => $serv)
+            {
+                if ($cat !== 'page_serv') {
+                    ?>
+                    <tr><td colspan="3"><h3><?php echo $cat; ?></h3></td></tr>
+                    <?php
+                    foreach ($serv as $name => $value) {
+                        ?>
+                        <tr>
+                        <td><?php echo $i; ?></td>
+                        <td style="text-align:left"><?php echo $name; ?></td>
+                        <td><?php echo $value; ?></td>
+                        </tr>
+                        <?php
+                        $i++;
+                    }
+                } else {
+                    ?>
+                    <tr><td colspan="3"><h3>Другие услуги</h3></td></tr>
+                    <?php
+                    foreach ($serv as $name => $value) {
+                        ?>
+                        <tr>
+                        <td><?php echo $i; ?></td>
+                        <td style="text-align:left"><?php echo $name; ?></td>
+                        <td><?php echo $value; ?></td>
+                        </tr>
+                        <?php
+                        $i++;
+                    }
+                }
+            }
+            ?>
+        </tbody>
+        </table>
+    </div>
+<?php
+}
+?>
 </div>
