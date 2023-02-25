8a02ff35f9bcee5338274834e51372361ecfc027
I-Jurij <yjurij@gmail.com>
2023-02-25 21:01:51 +0300

    appoint_by_master ended

--- app/models/appoint_by_master.php
+++ app/models/appoint_by_master.php
@@ -1,5 +1,62 @@
 <?php
 namespace App\Models;
+use \App\Lib\Registry;
 class Appoint_by_master extends Home
 {
+    protected function db_query()
+    {
+        //add data for head in template
+        if ($this->db->db->has($this->table, ["page_alias" => $this->page])) {
+        $this->data['page_db_data'] = $this->db->db->select($this->table, "*", ["page_alias" => $this->page]);
+        }
+        if (!empty($this->data['page_db_data'])) {
+            Registry::set('page_db_data', $this->data['page_db_data']);
+        }
+        $this->data['masters'] = $this->get_data_masters();
+        foreach ($this->data['masters'] as $master) {
+            $key = $master['master_name'] . ' ' . $master['sec_name'] . ' ' . $master['master_fam'] . '<br />'. phone_number_view($master['master_phone_number']);
+            $app[$key] = $this->get_data_app($master['id']);
+        }
+        foreach ($app as $master => $value) {
+            foreach ($value as $val) {
+                list($year, $month, $day) = explode('-', $val['den']);
+                $date =  $day.'.'.$month.'.'.$year.', '.$val['denned'];
+                $time = $val['vremia'];
+                $name = ($val['name_client'] != '&hellip;') ? $val['name_client'].', ' : '';
+                $this->data['app'][$master][$date][$time] = $val['usluga'].' руб.<br />'.$name.$val['tlf_client'];
+            }
+        }
+    }
+
+    public function get_data_masters()
+    {
+      $masters = $this->db->db->select("masters", ["id", "master_name", "sec_name", "master_fam", "master_phone_number"], [ "OR" => [ "data_uvoln" => "",  "data_uvoln[=]" => null ] ]);
+      return $masters;
+    }
+
+    protected function get_data_app($master_id){
+        $app = $this->db->db->select("app_to_".$master_id, [
+                "den",
+                "denned",
+                "vremia",
+                "usluga",
+                "name_client",
+                "tlf_client"
+        ], [
+            "AND" => [
+                "OR" => [
+                    "tlf_client[!]" => "",
+                    "tlf_client[!]" => null
+                ],
+                "den[>]" => date('Y-m-d')
+            ],
+            "ORDER" => [
+                "den" => "ASC",
+                "vremia" => "ASC"
+            ]
+        ]);
+        return $app;
+    }
+
+
 }
--- app/view/appoint_by_master.php
+++ app/view/appoint_by_master.php
@@ -1,3 +1,37 @@
-<div class="content">
-appoint_by_master.php
-</div>
+<?php
+if (!empty($data['app'])) {
+    print '<div class="content app">';
+    foreach ($data['app'] as $master => $value) {
+        print '<button type="button" class="buttons" id="'.sanitize(translit_ostslav_to_lat($master)).'" onclick="showZ(this.id);">'.$master.'</button>';
+    }
+    print '</div>';
+
+    foreach ($data['app'] as $mast => $date_arr) {
+        print '<div class="zapis_usluga display_none app" id="div_'.sanitize(translit_ostslav_to_lat($mast)).'">';
+            print '<div class="back shad rad pad margin_rlb1" >';
+                print '<span><b>'.$mast.'</b></span>';
+            print '</div>';
+            foreach ($date_arr as $date => $data_arr) {
+                print ' <div class="back shad rad pad margin_rlb1">
+                            <span class="display_inline_block zapis_usluga"><b>'.$date.'</b></span>';
+                foreach ($data_arr as $time => $data) {
+                    echo '  <div class="back shad rad pad mar display_inline_block">
+                                <span>'.$time.'</span><br />
+                                <span>'.$data.'</span>
+                            </div>';
+                }
+                print ' </div>';
+            }
+        print '</div >';
+    }
+} else {
+    print '<div class="content"><p>No data.</p></div>';
+}
+?>
+
+<script>
+function showZ(id){
+  $('.app').hide();
+  $('#div_'+id).toggle();
+}
+</script>
