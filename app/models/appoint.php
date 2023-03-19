fa33909aa459445af6e713514f478431990a9a77
I-Jurij <yjurij@gmail.com>
2023-03-19 19:28:02 +0300

    d

--- app/models/appoint.php
+++ app/models/appoint.php
@@ -1,9 +1,19 @@
 <?php
 namespace App\Models;
+use \App\Lib\Registry;
+
 class Appoint extends Home
 {
     protected function db_query()
 	{
+        //add data for head in template
+		if ($this->db->db->has($this->table, ["page_alias" => $this->page])) {
+		    $this->data['page_db_data'] = $this->db->db->select($this->table, "*", ["page_alias" => $this->page]);
+		}
+		if (!empty($this->data['page_db_data'])) {
+			Registry::set('page_db_data', $this->data['page_db_data']);
+		}
+
 		$data['service_page'] = $this->db->db->select($this->table, [
             "page_id",
             "page_alias",
